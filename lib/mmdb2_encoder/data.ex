1e09140cb5443d02bf8a4a38e0526e6253064064
Marc Neudert <marc.neudert@gmail.com>
2019-05-19 12:34:30 +0200

    Allow encoding binary values

--- lib/mmdb2_encoder/data.ex
+++ lib/mmdb2_encoder/data.ex
@@ -2,6 +2,7 @@ defmodule MMDB2Encoder.Data do
   @moduledoc false
 
   # standard data types
+  @binary 2
   @extended 0
 
   # extended data types
@@ -13,6 +14,20 @@ defmodule MMDB2Encoder.Data do
   Encodes a value to the appropriate MMDB2 representation.
   """
   @spec encode(term) :: binary
+  def encode(""), do: <<@binary::size(3), 0::size(5)>>
+
+  def encode(binary) when is_binary(binary) and byte_size(binary) >= 65_821,
+    do: <<@binary::size(3), 31::size(5), byte_size(binary) - 65_821::size(24), binary::binary>>
+
+  def encode(binary) when is_binary(binary) and byte_size(binary) >= 285,
+    do: <<@binary::size(3), 30::size(5), byte_size(binary) - 285::size(16), binary::binary>>
+
+  def encode(binary) when is_binary(binary) and byte_size(binary) >= 29,
+    do: <<@binary::size(3), 29::size(5), byte_size(binary) - 29::size(8), binary::binary>>
+
+  def encode(binary) when is_binary(binary),
+    do: <<@binary::size(3), byte_size(binary)::size(5), binary::binary>>
+
   def encode(true), do: <<@extended::size(3), 1::size(5), @extended_bool>>
   def encode(false), do: <<@extended::size(3), 0::size(5), @extended_bool>>
   def encode(:cache), do: <<@extended::size(3), 0::size(5), @extended_cache>>
--- test/mmdb2_encoder/data_test.exs
+++ test/mmdb2_encoder/data_test.exs
@@ -2,6 +2,11 @@ defmodule MMDB2Encoder.DataTest do
   use ExUnit.Case, async: true
 
   @test_values [
+    {"binary length 0", ""},
+    {"binary length 16", String.duplicate("x", 16)},
+    {"binary length 29", String.duplicate("x", 29)},
+    {"binary length 285", String.duplicate("x", 285)},
+    {"binary length 65_821", String.duplicate("x", 65_821)},
     {"boolean false", false},
     {"boolean true", true},
     {"cache marker", :cache},
