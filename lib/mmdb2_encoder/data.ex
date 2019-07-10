0139ea04e3483f58a98a419c742b7e82de7bb57a
Marc Neudert <marc.neudert@gmail.com>
2019-07-10 20:25:35 +0200

    Allow encoding with manual/automatic type selection

--- lib/mmdb2_encoder/data.ex
+++ lib/mmdb2_encoder/data.ex
@@ -10,26 +10,41 @@ defmodule MMDB2Encoder.Data do
   @extended_cache_container 5
   @extended_end_marker 6
 
+  @type datatype :: :binary | :boolean | :cache_container | :end_marker
+
+  @doc """
+  Encodes a value with automatic type selection.
+  """
+  @spec encode(value :: term) :: binary
+  def encode(value) when is_binary(value), do: encode(:binary, value)
+  def encode(value) when is_boolean(value), do: encode(:boolean, value)
+  def encode(:cache_container), do: encode(:cache_container, :cache_container)
+  def encode(:end_marker), do: encode(:end_marker, :end_marker)
+
   @doc """
   Encodes a value to the appropriate MMDB2 representation.
   """
-  @spec encode(term) :: binary
-  def encode(""), do: <<@binary::size(3), 0::size(5)>>
+  @spec encode(type :: datatype, value :: term) :: binary
+  def encode(:binary, ""), do: <<@binary::size(3), 0::size(5)>>
 
-  def encode(binary) when is_binary(binary) and byte_size(binary) >= 65_821,
+  def encode(:binary, binary) when is_binary(binary) and byte_size(binary) >= 65_821,
     do: <<@binary::size(3), 31::size(5), byte_size(binary) - 65_821::size(24), binary::binary>>
 
-  def encode(binary) when is_binary(binary) and byte_size(binary) >= 285,
+  def encode(:binary, binary) when is_binary(binary) and byte_size(binary) >= 285,
     do: <<@binary::size(3), 30::size(5), byte_size(binary) - 285::size(16), binary::binary>>
 
-  def encode(binary) when is_binary(binary) and byte_size(binary) >= 29,
+  def encode(:binary, binary) when is_binary(binary) and byte_size(binary) >= 29,
     do: <<@binary::size(3), 29::size(5), byte_size(binary) - 29::size(8), binary::binary>>
 
-  def encode(binary) when is_binary(binary),
+  def encode(:binary, binary) when is_binary(binary),
     do: <<@binary::size(3), byte_size(binary)::size(5), binary::binary>>
 
-  def encode(true), do: <<@extended::size(3), 1::size(5), @extended_boolean>>
-  def encode(false), do: <<@extended::size(3), 0::size(5), @extended_boolean>>
-  def encode(:cache_container), do: <<@extended::size(3), 0::size(5), @extended_cache_container>>
-  def encode(:end_marker), do: <<@extended::size(3), 0::size(5), @extended_end_marker>>
+  def encode(:boolean, true), do: <<@extended::size(3), 1::size(5), @extended_boolean>>
+  def encode(:boolean, false), do: <<@extended::size(3), 0::size(5), @extended_boolean>>
+
+  def encode(:cache_container, :cache_container),
+    do: <<@extended::size(3), 0::size(5), @extended_cache_container>>
+
+  def encode(:end_marker, :end_marker),
+    do: <<@extended::size(3), 0::size(5), @extended_end_marker>>
 end
--- test/mmdb2_encoder/data_test.exs
+++ test/mmdb2_encoder/data_test.exs
@@ -2,21 +2,25 @@ defmodule MMDB2Encoder.DataTest do
   use ExUnit.Case, async: true
 
   @test_values [
-    {"binary length 0", ""},
-    {"binary length 16", String.duplicate("x", 16)},
-    {"binary length 29", String.duplicate("x", 29)},
-    {"binary length 285", String.duplicate("x", 285)},
-    {"binary length 65_821", String.duplicate("x", 65_821)},
-    {"boolean false", false},
-    {"boolean true", true},
-    {"cache container", :cache_container},
-    {"end marker", :end_marker}
+    {"binary length 0", :binary, ""},
+    {"binary length 16", :binary, String.duplicate("x", 16)},
+    {"binary length 29", :binary, String.duplicate("x", 29)},
+    {"binary length 285", :binary, String.duplicate("x", 285)},
+    {"binary length 65_821", :binary, String.duplicate("x", 65_821)},
+    {"boolean false", :boolean, false},
+    {"boolean true", :boolean, true},
+    {"cache container", :cache_container, :cache_container},
+    {"end marker", :end_marker, :end_marker}
   ]
 
-  for {name, value} <- @test_values do
+  for {name, type, value} <- @test_values do
     test "encode decode cycle: #{name}" do
-      assert unquote(value) ==
-               unquote(value) |> MMDB2Encoder.Data.encode() |> MMDB2Decoder.Data.value(0, [])
+      encoded_auto = MMDB2Encoder.Data.encode(unquote(value))
+      encoded_fixed = MMDB2Encoder.Data.encode(unquote(type), unquote(value))
+
+      assert encoded_auto == encoded_fixed
+      assert unquote(value) == MMDB2Decoder.Data.value(encoded_auto, 0, [])
+      assert unquote(value) == MMDB2Decoder.Data.value(encoded_fixed, 0, [])
     end
   end
 end
