43bee704ece3ec850fd807994a82ca9b1ebcc7c7
Marc Neudert <marc.neudert@gmail.com>
2019-07-10 20:27:19 +0200

    Allow encoding byte values

--- test/mmdb2_encoder/data_test.exs
+++ test/mmdb2_encoder/data_test.exs
@@ -1,7 +1,7 @@
 defmodule MMDB2Encoder.DataTest do
   use ExUnit.Case, async: true
 
-  @test_values [
+  @autodetectables [
     {"binary length 0", :binary, ""},
     {"binary length 16", :binary, String.duplicate("x", 16)},
     {"binary length 29", :binary, String.duplicate("x", 29)},
@@ -13,13 +13,28 @@ defmodule MMDB2Encoder.DataTest do
     {"end marker", :end_marker, :end_marker}
   ]
 
-  for {name, type, value} <- @test_values do
-    test "encode decode cycle: #{name}" do
+  @undetectables [
+    {"bytes length 0", :bytes, ""},
+    {"bytes length 16", :bytes, String.duplicate("x", 16)},
+    {"bytes length 29", :bytes, String.duplicate("x", 29)},
+    {"bytes length 285", :bytes, String.duplicate("x", 285)},
+    {"bytes length 65_821", :bytes, String.duplicate("x", 65_821)}
+  ]
+
+  for {name, type, value} <- @autodetectables do
+    test "encode decode (auto) cycle: #{name}" do
       encoded_auto = MMDB2Encoder.Data.encode(unquote(value))
       encoded_fixed = MMDB2Encoder.Data.encode(unquote(type), unquote(value))
 
-      assert encoded_auto == encoded_fixed
       assert unquote(value) == MMDB2Decoder.Data.value(encoded_auto, 0, [])
+      assert unquote(value) == MMDB2Decoder.Data.value(encoded_fixed, 0, [])
+    end
+  end
+
+  for {name, type, value} <- @undetectables do
+    test "encode decode (auto) cycle: #{name}" do
+      encoded_fixed = MMDB2Encoder.Data.encode(unquote(type), unquote(value))
+
       assert unquote(value) == MMDB2Decoder.Data.value(encoded_fixed, 0, [])
     end
   end
