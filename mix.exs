22716b94f3d4dba60d71dfa191217cc3631e89d7
Marc Neudert <marc.neudert@gmail.com>
2020-09-13 12:00:29 +0200

    Remove deprecated hex :maintainers metadata

--- mix.exs
+++ mix.exs
@@ -56,8 +56,7 @@ defmodule MMDB2Encoder.MixProject do
     %{
       files: ["CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib"],
       licenses: ["Apache 2.0"],
-      links: %{"GitHub" => @url_github},
-      maintainers: ["Marc Neudert"]
+      links: %{"GitHub" => @url_github}
     }
   end
 end
